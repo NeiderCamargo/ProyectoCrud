b2b252f3c251fa29d6c2b5e0d3f516ed9d99af8f
neider_camargo <nfcamargo99@outlook.com>
2023-10-09 01:27:54 -0500

    2. Creamos una paquete que se llame product con una clase del mismo nombre. Inicializamos las variabels de los datos, creamos un metodo vacio, otro todas las variables y uno mas sin el ID, mas los metodos getters y setters y luego llamamos los datos de la siguiente manera en la clase principal

--- src/main/java/com/example/crud/CrudApplication.java
+++ src/main/java/com/example/crud/CrudApplication.java
@@ -1,11 +1,14 @@
 package com.example.crud;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import product.Product;
 
 @SpringBootApplication
 @RestController
@@ -14,14 +17,17 @@ public class CrudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
 	}
-
-	/* Creamos un metodo que nos devuelva nombres en string */
-	@GetMapping(path = "/") /* Con esto ponemos disponible para consulta */
-	public List<String> getNombres() {
-		return List.of(
-				"chis",
-				"Daniela",
-				"Neider");
-	}
-
+	
+    @GetMapping(path = "/")
+    public List<Product> getNombres(){
+        return List.of(
+            new Product(
+                3454,
+                "laptop",
+                4000f,
+                LocalDate.of(2033, 04, 04),
+                2
+            )
+        );
+    }
 }
