0cd3426052fb7ee9820218fe59fa1dd6b05a6567
neider_camargo <nfcamargo99@outlook.com>
2023-10-08 22:45:14 -0500

    ejemplo: metodo de consulta de nombres en string

--- src/main/java/com/example/crud/CrudApplication.java
+++ src/main/java/com/example/crud/CrudApplication.java
@@ -1,13 +1,27 @@
 package com.example.crud;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
+	}
+
+	/* Creamos un metodo que nos devuelva nombres en string */
+	@GetMapping(path = "/") /* Con esto ponemos disponible para consulta */
+	public List<String> getNombres() {
+		return List.of(
+				"chis",
+				"Daniela",
+				"Neider");
 	}
 
 }
