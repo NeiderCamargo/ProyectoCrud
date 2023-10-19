0d7336fc71d3844b2487a478fdc07271af6324f9
neider camargo <nfcamargo99@outlook.com>
2023-10-18 19:03:11 -0500

    eliminar proyecto crud y agregar proyecto crudinventario

---
+++ crudinventario/src/main/java/com/inventario/crudinventario/repository/ProductoRepository.java
@@ -0,0 +1,11 @@
+package com.inventario.crudinventario.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.inventario.crudinventario.entity.Producto;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+}
---
+++ crudinventario/src/test/java/com/inventario/crudinventario/CrudinventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventario.crudinventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudinventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
